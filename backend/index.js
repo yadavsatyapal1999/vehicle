2f93a4cb471c88024ce678e80c87f49e0f92e406
Satyapal Yadav <114231518+yadavsatyapal1999@users.noreply.github.com>
2024-05-23 20:34:34 +0000

    frontend created and Schema to add a vehicle completed

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const app = express();
+
+mongoose.connect("mongodb+srv://satyapalmechworld:axN0ykTi1TcZ18ED@cluster0.qkhyapj.mongodb.net/Vehicle_Booking?retryWrites=true&w=majority").then((resp)=>{
+    console.log('Connected to DB Sucessfully');
+    //console.log(resp)
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+app.use(cors({}));
+app.listen("12345",()=>{
+    console.log("Server listening at port 12345")
+})
